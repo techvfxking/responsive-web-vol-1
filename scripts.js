734f1fdd19d204c5427c28d6712c682b73822053
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-12-07 00:53:35 +0530

    Added Menu section

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 const menuOpenButton = document.getElementById("menu-open-button");
+const menuCloseButton = document.getElementById("menu-close-button");
 
 menuOpenButton.addEventListener("click", onMenuButtonOpenClick);
+menuCloseButton.addEventListener("click", onMenuButtonCloseClick);
+
 
 function onMenuButtonOpenClick() {
     document.body.classList.toggle("show-mobile-menu");
+}
+
+function onMenuButtonCloseClick() {
+   menuOpenButton.click();
 }
