95c41890b69a887e33beab7061192f125ce34038
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-12-08 00:41:01 +0530

    Stories Section

--- scripts.js
+++ scripts.js
@@ -10,5 +10,34 @@ function onMenuButtonOpenClick() {
 }
 
 function onMenuButtonCloseClick() {
-   menuOpenButton.click();
+    menuOpenButton.click();
 }
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween: 25,
+    grabCursor: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+
+        }
+    }
+});
