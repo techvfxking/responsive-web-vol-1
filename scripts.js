5c7bf51bc6780da5207cf9e974fb92bcca2e11e1
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-12-15 00:57:18 +0530

    Committing Final changes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const navlinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.getElementById("menu-open-button");
 const menuCloseButton = document.getElementById("menu-close-button");
 
@@ -12,6 +13,12 @@ function onMenuButtonOpenClick() {
 function onMenuButtonCloseClick() {
     menuOpenButton.click();
 }
+
+navlinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+        onMenuButtonCloseClick();
+    });
+});
 
 const swiper = new Swiper('.slider-wrapper', {
     loop: true,
